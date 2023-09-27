aec9e4e606faee1615181d1a0d208d841c4b6e4c
aftar51 <afftardarda@gmail.com>
2023-09-27 20:38:32 +0700

    changes

--- js/index.js
+++ js/index.js
@@ -44,7 +44,7 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 const typed = new Typed('.multiple-text',{
-    strings: ['Frontend Developer', 'roblox developer','student of IDN bording scholl ',],
+    strings: ['Frontend Developer', 'roblox developer','student of IDN bording school ',],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
